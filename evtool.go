c68dd41cf4e5ffd7e4b670f56b75e851e5a11846
Daniel Arenas <danarenasb@gmail.com>
2021-06-03 20:51:14 -0500

    added comments

--- evtool.go
+++ evtool.go
@@ -8,6 +8,7 @@ import (
 
 var emailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
+// IsEmailValid takes the email as a string and checks for 3 thingsL 1)length (min 3 max 254), 2) that it macthes REGEX "^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$") and 3) uses http package to check DNX MX records for the domain.
 func IsEmailValid(e string) bool {
 	if len(e) < 3 && len(e) > 254 {
 		return false
@@ -23,6 +24,7 @@ func IsEmailValid(e string) bool {
 	return true
 }
 
+// NormalizeGmail removes all periods and anything after the + sign on gmail addresses, as these are considered ways to create aliases for one gmail address
 func NormalizeGmail(e string) string {
 	emailWithoutDots := strings.ReplaceAll(e, ".", "")
 	if strings.Contains(emailWithoutDots, "+") {
